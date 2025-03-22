429840dc73ace5d8cc2448fac21de870450f0e41
Adnan <adnanxyz06@gmail.com>
2025-03-22 05:41:26 +0530

    Added backend server

---
+++ server/src/index.js
@@ -0,0 +1,30 @@
+
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db.config');
+
+
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors());
+
+
+connectDB();
+
+//Routes
+
+
+app.get('/', (req,res) => {
+  res.send('Express  Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
